f3f4877886df1287754cf047aeb3bde6c44554d9
anacanm <anacan@mangelsdorf.io>
2020-05-20 20:53:30 -0700

    Add documentation for fileShouldBeWatched

--- cmd/fileutil.go
+++ cmd/fileutil.go
@@ -2,6 +2,9 @@ package main
 
 import "strings"
 
+// fileShouldBeWatched is a helper function that returns a boolean
+// returns whether or not the file extension is in the acceptedFileExtensions string slice
+// TODO: Add testing
 func fileShouldBeWatched(fileName string, acceptedFileExtensions []string) bool {
 	for _, v := range acceptedFileExtensions {
 		if strings.HasSuffix(fileName, v) {
