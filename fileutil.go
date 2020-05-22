94f2a32e8c3754a9857a1901f62ee639d1af2b2a
anacanm <anacan@mangelsdorf.io>
2020-05-22 16:00:59 -0700

    Add getFilesToRun, returns a string slice of go filenames to be run
    getFilesToRun returns a string slice of go filenames in the specified (default: root of where gomon is being run) directory that should be run

--- fileutil.go
+++ fileutil.go
@@ -1,6 +1,11 @@
 package main
 
-import "strings"
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+)
 
 // fileShouldBeWatched is a helper function that returns a boolean
 // returns whether or not the file extension is in the acceptedFileExtensions string slice
@@ -13,6 +18,7 @@ func fileShouldBeWatched(fileName string, acceptedFileExtensions []string) bool 
 	return false
 }
 
+// filterOutTests returns a NEW string slice of go filenames, with test files filtered out
 func filterOutTests(filesWithTests []string) []string {
 	// initialize result with a capacity of the length of filesWith Tests
 	result := make([]string, 0, len(filesWithTests))
@@ -25,4 +31,37 @@ func filterOutTests(filesWithTests []string) []string {
 	}
 
 	return result
+}
+
+// getFilesToRun returns a string slice of go filenames in the specified (default: root of where gomon is being run) directory that should be run
+func getFilesToRun() []string {
+	// goFileMatches is a slice of strings that are the names of the files in the specified directory that are go files that need to be run
+	var filePathToLookForGoFiles string
+	if len(os.Args) > 1 {
+		// the user has the option to specify where go run should be ran (ex, cmd)
+		// if they did provide at least one command line arg, then we need to check to see if a file location was provided
+		filePathToLookForGoFiles = os.Args[1]
+
+		// if the filepath provided by the user does not have a "/" at the end (as it should) to specify that it is a dir
+		// then add the "/"
+		if !strings.HasSuffix(filePathToLookForGoFiles, "/") {
+			filePathToLookForGoFiles += "/"
+		}
+
+		// if the first command line argument begins with a "-", then it is a flag, and gomon should be run in the root dir
+		if strings.HasPrefix(filePathToLookForGoFiles, "-") {
+			filePathToLookForGoFiles = ""
+		}
+
+	}
+	// find all files that have the .go file extensions where the user specified to look
+	// we need to run them like "go run main.go flagutil.go fileutil.go", creating our own wildcard (*) functionality
+	goFileMatches, err := filepath.Glob(filePathToLookForGoFiles + "*.go")
+	if err != nil {
+		fmt.Printf("Error getting filematches: %v", err)
+	}
+
+	goFileMatchesWithoutTests := filterOutTests(goFileMatches)
+
+	return goFileMatchesWithoutTests
 }
